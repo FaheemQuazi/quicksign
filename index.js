c3a323795e1e7fd71f79fc70c66cd7b39619d0d6
Faheem Quazi <faheemquazi@gmail.com>
2020-12-13 01:08:24 -0600

    remove electron dependency (no support on armel)

--- index.js
+++ index.js
@@ -1,61 +1,45 @@
-const { app, BrowserWindow } = require('electron');
-const settings = require('electron-app-settings');
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const multer = require('multer');
 
-const webctrl = express();
+const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var upload = multer({dest: 'upload/'});
 
 var win;
-var fname = settings.get('disp') || 'index.html';
+var fname = 'default';
 
-if (!fs.existsSync(__dirname + 'upload/')) {
-    fs.mkdirSync(__dirname + 'upload/');
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'static')));
+
+if (fs.existsSync(path.join(__dirname, 'upload'))) {
+    fs.rmdirSync(path.join(__dirname, 'upload'), {force: true});
+}
+fs.mkdirSync(path.join(__dirname, 'upload'));
+
+if (fs.existsSync(path.join(__dirname, 'views/custom.ejs'))) {
+    fname = 'custom';
 }
 
-if (fname == 'index.html') {
-
-}
-
-app.whenReady().then(() => {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        },
-        //fullscreen: true,
-        //kiosk: true,
-        autoHideMenuBar: true
+app.get('/', (req, res) => {
+    res.render('index', {
+        fname: fname
     });
-
-    win.loadFile(fname);
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
-
-webctrl.get('/', (req, res) => {
+app.get('/edit', (req, res) => {
     res.send('<form action="/up" method="post" enctype="multipart/form-data"><input type="file" name="newsite" /><input type="submit" value="Update" name="sub"/></form>');
 });
 
-webctrl.post('/up', upload.single('newsite'), (req, res, next) => {
-    fs.renameSync(req.file.path, 'upload\\' + req.file.filename + '.html');
-    win.loadFile('upload\\' + req.file.filename + '.html');
-    settings.set("disp", 'upload\\' + req.file.filename + '.html');
-    res.redirect('/');
+app.post('/up', upload.single('newsite'), (req, res, next) => {
+    fs.renameSync(req.file.path, 'views\\custom.ejs');
+    fname = "custom";
+    io.emit('ref');
+    res.redirect('/edit');
 });
 
-webctrl.listen(3003, () => {
+http.listen(3003, () => {
     console.log("listening");
 });
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # QuickSign
 
-Made this because I needed to put customizable HTML-based content onto a display running on a Raspberry Pi.
+Made this because I needed to put customizable HTML-based content onto a display running on a Raspberry Pi Zero W.
 
 Warning: probably really bad. I wrote this in like two hours. Will update as needed.
